08a2c81dae98c9d2bcfb324c54c0603c827f54aa
Hannah Mackaness <hannah.mackaness@unrulygroup.com>
2017-07-14 11:09:12 +0100

    fixed first set of buttons, second set now more broken - adding hidden class not working on forward button

--- carousel.js
+++ carousel.js
@@ -2,10 +2,10 @@
 console.log("test test test")
 //select all images
 var carObjects = document.getElementsByClassName("li_1");
-console.log(carObjects[1]);
- var icounter = 0;
-console.log(carObjects.length);
-//setting hidden
+//reminder carObjects is an array --> console.log(carObjects[1]);
+var icounter = 0;
+
+//set hidden to all but first item in image array
 for (i=1; i< carObjects.length; i++) {
 	var individualObject = carObjects[i];
 	individualObject.classList.add("hidden");
@@ -15,11 +15,7 @@ for (i=1; i< carObjects.length; i++) {
 var buttons = '<div id="controls"><p id="control-prev" class="btn btn-primary">Previous slide</p><p id="control-next" class="btn btn-primary">Next slide</p></div>'
 carousel.insertAdjacentHTML("afterend", buttons)
 
-
-
-	
-
-//working loop	
+//working loop for button functionaity first forwards set
 document.getElementById("control-next").addEventListener(
 	"click",
 	function goingforwards(event) {
@@ -27,30 +23,19 @@ document.getElementById("control-next").addEventListener(
 		carObjects[icounter].classList.add("hidden")
 		icounter=(icounter+1)%6;
 		carObjects[icounter].classList.remove("hidden")
-
-		//console.log(icounter);
-		//console.log(i);
-		//console.log(carObjects[icounter]);
-	}
+	    }
 	);	
 
-//working - no looping
+//working loop for button functionaity first backawards set
 
 document.getElementById("control-prev").addEventListener(
 	"click",
 	function goingBackwards(event) {
 		carObjects[icounter].classList.add("hidden")
 		icounter=((icounter-1)+6)%6;
-		//console.log(icounter);
 		carObjects[icounter].classList.remove("hidden")
-		// console.log(icounter);
-		// console.log(i);
-		// console.log(carObjects[icounter]);
-		//shows the next item
-	
-	}
-
-	);
+	    }
+    );
 
 // modular division of this instance works as you have (for forwards), at place 3
 //3%6 = 3 so counter is at 3. There is no need to cap to avoid -11 etc bc when
@@ -58,7 +43,7 @@ document.getElementById("control-prev").addEventListener(
 //it's harder going backwards becasue you get to your last good one (0) and THAT'S
 //the trigger to go the the last photo so you have (0)-1 = -1 + 6 = 5%6. You go back to 5 
 //but 'up' to 0. 
-
+//---- show/hide experiment ----//
 // var unit=document.getElementById("carousel");
 // var title = 
 // console.log(unit);
@@ -86,8 +71,7 @@ document.getElementById("control-prev").addEventListener(
 //---------work for SECOND SET of everything----------------//
 //hide all irrelevant objects
 var carObjects2 = document.getElementsByClassName("li_2");
-//console.log(carObjects[1]);
- var icounter = 0;
+var icounter_2 = 0;
 console.log(carObjects2.length);
 //setting hidden
 for (i=1; i< carObjects2.length; i++) {
@@ -105,8 +89,8 @@ document.getElementById("control-next2").addEventListener(
 	function goingforwards(event) {
 				//shows the next item
 		carObjects2[icounter].classList.add("hidden")
-		icounter=(icounter+1)%6;
-		carObjects2[icounter].classList.remove("hidden")
+		icounter_2=(icounter_2+1)%6;
+		carObjects2[icounter_2].classList.remove("hidden")
 
 		//console.log(icounter);
 		//console.log(i);
@@ -118,10 +102,10 @@ document.getElementById("control-next2").addEventListener(
 document.getElementById("control-prev2").addEventListener(
 	"click",
 	function goingBackwards(event) {
-		carObjects2[icounter].classList.add("hidden")
-		icounter=((icounter-1)+6)%6;
+		carObjects2[icounter_2].classList.add("hidden")
+		icounter_2=((icounter_2-1)+6)%6;
 		//console.log(icounter);
-		carObjects2[icounter].classList.remove("hidden")
+		carObjects2[icounter_2].classList.remove("hidden")
 		// console.log(icounter);
 		// console.log(i);
 		// console.log(carObjects[icounter]);
